145cd347ad113e4e0f85174beaf2f614c200e7ff
Stefan <stefanbitcr@gmail.com>
2025-06-10 07:52:55 +0200

    Factor out shared swap

--- crates/bcr-wallet-core/src/wallet/api.rs
+++ crates/bcr-wallet-core/src/wallet/api.rs
@@ -1,7 +1,7 @@
 // ----- standard library imports
 // ----- extra library imports
-use cashu::{CheckStateRequest, ProofsMethods};
-use tracing::warn;
+use cashu::{Amount, CheckStateRequest, Proof, ProofsMethods, amount};
+use tracing::{error, warn};
 // ----- local modules
 use super::types::SwapProofs;
 use super::{utils, wallet::*};
@@ -228,5 +228,75 @@ where
         }
         warn!("Could not select subset of proofs to send");
         Err(anyhow::anyhow!("Could not select subset of proofs to send"))
+    }
+
+    pub async fn perform_swap(
+        &self,
+        proofs: Vec<Proof>,
+        amounts: Vec<Amount>,
+        keyset_id: cashu::Id,
+    ) -> anyhow::Result<Vec<Proof>> {
+        let wdc = &self.connector;
+
+        if proofs.is_empty() {
+            warn!("No proofs provided");
+            return Err(anyhow::anyhow!("No proofs provided"));
+        }
+
+        let mut total_proofs = 0u64;
+        let mut total_amounts = 0u64;
+        for p in &proofs {
+            total_proofs += u64::from(p.amount);
+        }
+        for a in &amounts {
+            total_amounts += u64::from(*a);
+        }
+        if total_proofs != total_amounts {
+            error!("Proofs and amounts do not match");
+            return Err(anyhow::anyhow!("Proofs and amounts do not match"));
+        }
+
+        let keys = wdc.list_keys(keyset_id).await?;
+        let keys = keys
+            .keysets
+            .first()
+            .ok_or(anyhow::anyhow!("No keys found"))?;
+
+        let counter = self.db.get_count(keyset_id).await.unwrap_or(0);
+        let target = amount::SplitTarget::Values(amounts);
+        let premint_secrets = cashu::PreMintSecrets::from_xpriv(
+            keyset_id,
+            counter,
+            self.xpriv,
+            Amount::from(total_proofs),
+            &target,
+        )?
+        .secrets;
+
+        let bs = premint_secrets
+            .iter()
+            .map(|b| b.blinded_message.clone())
+            .collect::<Vec<_>>();
+        let swap_request = cashu::nut03::SwapRequest::new(proofs, bs);
+
+        let response = wdc.swap(swap_request).await?;
+
+        let secrets = premint_secrets
+            .iter()
+            .map(|b| b.secret.clone())
+            .collect::<Vec<_>>();
+        let rs = premint_secrets
+            .iter()
+            .map(|b| b.r.clone())
+            .collect::<Vec<_>>();
+
+        let proofs = cashu::dhke::construct_proofs(response.signatures, rs, secrets, &keys.keys)?;
+
+        let _ = self
+            .db
+            .increase_count(keyset_id, proofs.len() as u32)
+            .await?;
+
+        Ok(proofs)
     }
 }
--- crates/bcr-wallet-core/src/wallet/credit.rs
+++ crates/bcr-wallet-core/src/wallet/credit.rs
@@ -1,8 +1,8 @@
 // ----- standard library imports
 // ----- extra library imports
 use anyhow::Result;
-use cashu::{Amount, Proof, amount};
-use tracing::{error, info, warn};
+use cashu::{Amount, Proof};
+use tracing::{debug, error};
 // ----- local modules
 use super::types::SwapProofs;
 use super::wallet::*;
@@ -74,69 +74,18 @@ impl<DB: WalletDatabase + KeysetDatabase> SwapProofs for Wallet<CreditWallet, DB
         proofs: Vec<Proof>,
         amounts: Vec<Amount>,
     ) -> Result<Vec<Proof>> {
-        let wdc = &self.connector;
-        if proofs.is_empty() {
-            warn!("No proofs provided");
-            return Err(anyhow::anyhow!("No proofs provided"));
-        };
-        let mut total_proofs = 0u64;
-        let mut total_amounts = 0u64;
-        for p in &proofs {
-            total_proofs += u64::from(p.amount);
-        }
-        for a in &amounts {
-            total_amounts += u64::from(*a);
-        }
-        if total_proofs != total_amounts {
-            error!("Proofs and amounts do not match");
-            return Err(anyhow::anyhow!("Proofs and amounts do not match"));
-        }
-
+        // Swap to the same keyset
         let keyset_id = proofs[0].keyset_id;
-        info!(keyset_id=?keyset_id, "Swap");
 
-        let keys = wdc.list_keys(keyset_id).await?;
+        // check that all proofs have the same keyset
+        for p in &proofs {
+            if p.keyset_id != keyset_id {
+                error!("Proofs do not belong to the same keyset");
+                return Err(anyhow::anyhow!("Proofs do not belong to the same keyset"));
+            }
+        }
 
-        let keys = keys
-            .keysets
-            .first()
-            .ok_or(anyhow::anyhow!("No keys found"))?;
-
-        let counter = self.db.get_count(keyset_id).await.unwrap_or(0);
-        let target = amount::SplitTarget::Values(amounts);
-        let premint_secrets = cashu::PreMintSecrets::from_xpriv(
-            keyset_id,
-            counter,
-            self.xpriv,
-            Amount::from(total_proofs),
-            &target,
-        )?
-        .secrets;
-
-        let bs = premint_secrets
-            .iter()
-            .map(|b| b.blinded_message.clone())
-            .collect::<Vec<_>>();
-        let swap_request = cashu::nut03::SwapRequest::new(proofs, bs);
-
-        let response = wdc.swap(swap_request).await?;
-
-        let secrets = premint_secrets
-            .iter()
-            .map(|b| b.secret.clone())
-            .collect::<Vec<_>>();
-        let rs = premint_secrets
-            .iter()
-            .map(|b| b.r.clone())
-            .collect::<Vec<_>>();
-
-        let proofs = cashu::dhke::construct_proofs(response.signatures, rs, secrets, &keys.keys)?;
-
-        let _ = self
-            .db
-            .increase_count(keyset_id, proofs.len() as u32)
-            .await?;
-
-        Ok(proofs)
+        debug!(keyset_id = ?keyset_id, amounts=?amounts,"Swapping credit proofs");
+        self.perform_swap(proofs, amounts, keyset_id).await
     }
 }
